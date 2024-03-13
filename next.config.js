6c868562db6c5fde2092568107cdad68d49da6e3
Tom Robshaw <t.robshaw@outlook.com>
2024-03-14 10:46:03 +1100

    returned to latest versions removing static html

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Other configuration options...
-
-  // Add the output configuration for static HTML export
-  output: 'export',
-
-  // Add the exportPathMap function for static HTML export
-  exportPathMap: function () {
-    return {
-      '/': { page: '/' },
-      // Add other routes as needed
-    };
-  },
 };
 
 module.exports = nextConfig;
